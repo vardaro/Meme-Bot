ff6223e085434ac87dbca551a387250a5b03512c
vardaro <anthonyavardaro@yahoo.com>
2017-02-28 11:36:01 -0600

    add tweet and img download functionality

--- src/main/java/TwitterBot.java
+++ src/main/java/TwitterBot.java
@@ -26,10 +26,11 @@ import java.util.Scanner;
 public class TwitterBot {
   private static final String[] SUBREDDITS_TO_MONITOR = {"me_irl", "WholesomeMemes"};
   private static final int LIMIT_OF_SUBMISSIONS = 25;
-  private static final String IMG_DIRECTORY = "C:\\Users\\justa\\IdeaProjects\\TwitterBot\\src\\img";
-  private static final String GRAVEYARD = "C:\\Users\\justa\\IdeaProjects\\TwitterBot\\src\\graveyard.txt";
+  private static final String IMG_DIRECTORY = "\\TwitterBot\\src\\img";
+  private static final String GRAVEYARD = "\\TwitterBot\\src\\graveyard.txt";
   public static void main(String[] args) throws TwitterException, OAuthException, InterruptedException {
     while(true) {
+      // Run through both subreddits before taking a nap
       for (String aSUBREDDITS_TO_MONITOR : SUBREDDITS_TO_MONITOR) {
         Twitter twitter = connectTwitter();
         RedditClient redditClient = connectReddit();
@@ -39,38 +40,43 @@ public class TwitterBot {
         sp.next(true);
         Listing<Submission> list = sp.getCurrentListing();
         for (int i = 0; i < LIMIT_OF_SUBMISSIONS; i++) {
-          //System.out.println(list.get(i).getTitle() + " " + list.get(i).getUrl());
-          //System.out.println(list.get(i));
-          String imgPath = getImage(list.get(i).getUrl());
+          System.out.println("[bot] Finding a post...");
           if (postHasNotBeenPosted(list.get(i))) {
+            System.out.println("[bot] ...Post found");
+            String imgPath = getImage(list.get(i).getUrl());
             String tweet = list.get(i).getTitle();
             File image = new File(imgPath);
 
+            // Tweet with media
             System.out.println("[bot] Tweeting: " + tweet + " with image " + imgPath);
             StatusUpdate status = new StatusUpdate(tweet);
             status.setMedia(image);
             twitter.updateStatus(status);
+
+            // Wait one minute before tweeting again
+            System.out.println("[bot] Sleeping for 30 seconds");
+            Thread.sleep(30000);
           }
-          // Wait two minutes before tweeting again
-          Thread.sleep(120000);
         }
       }
+      System.out.println("[bot] Sleeping for 12 hours");
       Thread.sleep(1000 * 60 * 60 * 12);
     }
   }
 
+  // Connects to Twitter API
   private static Twitter connectTwitter() {
     System.out.println("[bot] Connecting to Twitter...");
     Twitter twitter = new TwitterFactory().getSingleton();
     System.out.println("[bot] ...Successfully connected to Twitter");
     return twitter;
   }
-
+  // Connects to Reddit API
   private static RedditClient connectReddit() throws OAuthException {
     System.out.println("[bot] Connecting to Reddit...");
-    UserAgent myUserAgent = UserAgent.of("desktop", "bot", "v0.1", "TheItalipino");
+    UserAgent myUserAgent = UserAgent.of("desktop", "bot", "v0.1", "username");
     RedditClient redditClient = new RedditClient(myUserAgent);
-    Credentials credentials = Credentials.script("TheItalipino", "Reba321!", "skSYmiLn2tfQ5A", "Fe8DtvU-A4p-N3SFMX3WouMjdkY");
+    Credentials credentials = Credentials.script("username", "password", "token", "secret");
     OAuthData authData = redditClient.getOAuthHelper().easyAuth(credentials);
     redditClient.authenticate(authData);
     System.out.println("[bot] ...Successfully connected to Reddit");
@@ -78,40 +84,48 @@ public class TwitterBot {
   }
 
   private static String getImage(String link) {
-    // Method downloads each image to the 'img' directory
-    String filePath = null;
-    String fileName;
-    String extension;
-    try {
-      // Images from https://i.reddituploads.com/ are stupid and the '&'s get replaced replaced with '&amp;' which is the HTML entity for the '&' symbol.
-      link = link.replaceAll("&amp;","&");
+    // Check if the link has an image
+    if(link.contains("i.reddituploads.com") || link.contains("imgur.com") || link.contains("i.redd.it")) {
+      // Method downloads each image to the 'img' directory
+      String filePath = null;
+      String fileName;
+      String extension;
+      try {
+        // Images from https://i.reddituploads.com/ are stupid and the '&'s get replaced replaced with '&amp;'
+        // which is the HTML entity for the '&' symbol.
+        link = link.replaceAll("&amp;","&");
 
-      // Determine the file extension
-      extension = ".jpg";
-      if (link.contains(".png")) {
-        extension = ".png";
+        // Determine the file extension
+        extension = link.contains(".png") ? ".png" : ".jpg";
+
+        // This is so ugly, there is probably a better way to remove all the illegal chars from a string
+        fileName = link.substring(link.lastIndexOf("/")).replaceAll(extension, "").replace('?','r');
+        filePath = IMG_DIRECTORY + fileName + extension;
+        System.out.println("[bot] Downloading " + extension + " image at " + link + " as " + filePath);
+
+        // Download image to 'img' directory
+        // Credit to http://www.technicalkeeda.com/java-tutorials/ for code that downloads image to directory
+        URL url = new URL(link);
+        InputStream inputStream = url.openStream();
+        OutputStream outputStream = new FileOutputStream(filePath);
+        byte[] buffer = new byte[2048];
+        int length;
+        while((length = inputStream.read(buffer)) != -1) {
+          outputStream.write(buffer, 0, length);
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
       }
-
-      // This is so ugly, there has to be a better way to do this :/
-      fileName = link.substring(link.lastIndexOf("/")).replaceAll(extension, "").replace('?','r');
-      filePath = IMG_DIRECTORY + fileName + extension;
-      System.out.println("[bot] Downloading " + extension + " image at " + link + " as " + filePath);
-
-      // Download image to 'img; directory
-      // Credit to http://www.technicalkeeda.com/java-tutorials/ for bit that downloads image to directory
-      URL url = new URL(link);
-      InputStream inputStream = url.openStream();
-      OutputStream outputStream = new FileOutputStream(filePath);
-      byte[] buffer = new byte[2048];
-      int length;
-      while((length = inputStream.read(buffer)) != -1) {
-        outputStream.write(buffer, 0, length);
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+      return filePath;
     }
-    return filePath;
+    // If we couldn't find and image return null
+    System.out.println("[bot] No image found");
+    return null;
   }
+
+  // Every post on Reddit contains a unique post ID.
+  // This method determines the post ID and checks if it has been posted before.
+  // If it has not been posted before, write the ID to the .txt file to prevent duplicate tweets
   private static boolean postHasNotBeenPosted(Submission s) {
     File graveyard = new File(GRAVEYARD);
     PrintWriter out = null;
